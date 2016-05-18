9d3792ecb7b9d9b2b3add9dcec4c2c4dbfdd0ada
AsukaNakagawa <nina-jamie.elle14@live.jp>
2016-05-18 12:18:21 -0700

    my initial commit

---
+++ LHL-InstaKilo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LHL-InstaKilo
+//
+//  Created by Asuka Nakagawa on 2016-05-18.
+//  Copyright © 2016 Asuka Nakagawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
